325c24c545d59c7670dce69c313d84905cef660c
samuel.muller <samuel.muller207@gmail.com>
2021-02-25 11:34:10 -0300

    swagger configuration and descriptions implemented

--- src/main/java/br/com/muller/beerstock/controller/BeerControllerDocs.java
+++ src/main/java/br/com/muller/beerstock/controller/BeerControllerDocs.java
@@ -3,14 +3,39 @@ package br.com.muller.beerstock.controller;
 import br.com.muller.beerstock.dto.BeerDTO;
 import br.com.muller.beerstock.exception.BeerAlreadyRegisteredException;
 import br.com.muller.beerstock.exception.BeerNotFoundException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+@Api("Manages beer stock")
 public interface BeerControllerDocs {
 
+    @ApiOperation("Beer creation operation")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Success beer creation"),
+            @ApiResponse(code = 400, message = "Missing required fields or wrong field range value.")
+    })
     BeerDTO createBeer(BeerDTO beerDTO) throws BeerAlreadyRegisteredException;
+
+    @ApiOperation("Returns a beer found by a given name")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success beer found in the system"),
+            @ApiResponse(code = 404, message = "Beer with given name not found.")
+    })
     BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException;
+
+    @ApiOperation("List of all beers registered in the system")
+    @ApiResponse(code = 200, message = "")
     List<BeerDTO> listBeers();
+
+    @ApiOperation("Delete a beer found by a given valid Id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Success beer deleted in the system"),
+            @ApiResponse(code = 404, message = "Beer with given id not found.")
+    })
     void deleteById(@PathVariable Long id) throws BeerNotFoundException;
 }
